3e7119a6f5a0ef33e6cf6afc3b53a857f5485fb7
youngfram CH <thanapognchunchombun@gmail.com>
2024-09-05 18:04:49 +0700

    Edit Quotation Page

--- src/app/(protected)/income/quotation/edit-quotation/[id]/page.tsx
+++ src/app/(protected)/income/quotation/edit-quotation/[id]/page.tsx
@@ -10,26 +10,44 @@ import NewProductItem from "@/components/forms/income/newProductItem";
 import { Quotation, useDatabaseContext } from "@/contexts/dbContext";
 import { useEffect } from "react";
 import { useProductServiceListContext } from "@/contexts/productServiceListContext";
-import { Product, FormDataFooter } from '../../../../../../contexts/productServiceListContext';
+import {
+  Product,
+  FormDataFooter,
+} from "../../../../../../contexts/productServiceListContext";
 
 function EditQuotation({ params }: { params: { id: string } }) {
-  const { qoutationState } = useDatabaseContext();
-  const { setHeadForm, setProducts, setFooterForm } = useProductServiceListContext();
+  const { qoutationState, setEditQuotation } = useDatabaseContext();
+  const { setHeadForm, setProducts, setFooterForm } =
+    useProductServiceListContext();
 
-  const findObjectByKeyId = (data: Quotation[], keyId: string) => {
-    return data.find((item) => item.keyId === keyId);
+  const findObjectByKeyId = (
+    data: Quotation[],
+    keyId: string
+  ): Quotation | undefined => {
+    let _quotation = data.find((item) => item.keyId === keyId);
+
+    if (_quotation) {
+      return _quotation;
+    } else {
+      return;
+    }
   };
 
   useEffect(() => {
-    console.log(params.id);
+    // console.log(params.id);
     const result = findObjectByKeyId(qoutationState, params.id);
-    if (result?.headForm && result?.products && result?.summary) {
-      const {headForm, products, summary} = result;
-      setHeadForm(headForm);
-      setProducts(products)
-      setFooterForm(summary)
+
+    if (result) {
+      setEditQuotation(result);
     }
-    console.log(result)
+
+    if (result?.headForm && result?.products && result?.summary) {
+      const { headForm, products, summary } = result;
+      setHeadForm(headForm);
+      setProducts(products);
+      setFooterForm(summary);
+    }
+    console.log(result);
   }, []);
 
   return (
@@ -41,13 +59,13 @@ function EditQuotation({ params }: { params: { id: string } }) {
               <HeaderForm />
             </Grid>
             <Grid item xs={12} lg={12}>
-              <NewProductItem />
+              <NewProductItem isEdit={true} />
             </Grid>
             <Grid item xs={12} lg={12}>
               <ProductsServicesList />
             </Grid>
             <Grid item xs={12} lg={12}>
-              <FooterForm isEdit={true}/>
+              <FooterForm isEdit={true} />
             </Grid>
           </Grid>
         </Box>
